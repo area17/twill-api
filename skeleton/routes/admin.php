1b3722d492676250a5c1606d93e3264eba7775d2
Patrick Vézina <patrick@croustille.io>
2021-08-25 15:44:02 -0400

    Add Pages navigation to Twill admin

--- skeleton/routes/admin.php
+++ skeleton/routes/admin.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 // Register Twill routes here eg.
 // Route::module('posts');
 
+Route::module('pages');
+
 Route::module('books');
 
 Route::module('authors');
