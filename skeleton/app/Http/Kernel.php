0898f0f71ce73b8f7d246f1e3bc3e1308eb8f871
Patrick Vézina <patrick@croustille.io>
2021-08-26 11:44:26 -0400

    Remove SubstitureBinding from api middleware

--- skeleton/app/Http/Kernel.php
+++ skeleton/app/Http/Kernel.php
@@ -41,7 +41,6 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \A17\Twill\API\Http\Middleware\SetLocale::class,
         ],
     ];
