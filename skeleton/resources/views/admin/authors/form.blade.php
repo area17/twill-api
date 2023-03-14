920a7d5ed3030e5f6dab2148debe6fd272960538
Patrick Vézina <patrick@croustille.io>
2023-03-14 12:32:54 -0400

    Removed non-working tests

--- skeleton/resources/views/admin/authors/form.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('twill::layouts.form')
-
-@section('contentFields')
-    @formField('input', [
-        'name' => 'biography',
-        'label' => 'Biography',
-        'maxlength' => 100
-    ])
-@stop
--- skeleton/resources/views/admin/settings/general.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('twill::layouts.settings')
-
-@section('contentFields')
-    @formField('input', [
-        'label' => 'Site title',
-        'name' => 'site_title',
-        'textLimit' => '80'
-    ])
-@stop
